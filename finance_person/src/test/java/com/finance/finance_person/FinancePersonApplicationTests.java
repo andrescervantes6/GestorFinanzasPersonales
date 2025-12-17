62040bd64c79b55952833cb01470d1ecb596b5d2
Andres06 <andresmauriciocervantes8@gmail.com>
2025-12-16 21:46:43 -0500

    se agrega el proyecto

---
+++ finance_person/src/test/java/com/finance/finance_person/FinancePersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.finance_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
